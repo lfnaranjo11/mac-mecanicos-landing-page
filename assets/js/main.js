ef9ff7a1914fed360c86b1abc23df781837bd198
unknown <ma.carreno@uniandes.edu.co>
2020-07-22 17:45:45 -0500

    cambios de una semana

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,6 +142,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
+    autoplayTimeout: 10000,
     dots: true,
     loop: true,
     items: 1
